a5ee9bdc436259dab4232dd5e506c3711d193ae9
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-01-01 20:46:39 +0200

    schema referential done

--- ourDB/schema.sql
+++ ourDB/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS "PARTNER" (
 	"specialisation" integer,
 	"comments" string,
 	CONSTRAINT "check_specialisation" CHECK ("specialisation" BETWEEN 1 AND 4), --Έλεγχος για το πεδίο specialisation
-	PRIMARY KEY ("Tax_Id")
+	PRIMARY KEY ("Tax_Id") --οχι null το Tax_Id
 );
 
 -- δεν διαγράφουμε ποτέ partner, γιατί πρέπει να κραταμε αρχειο με ολα τα συμβολαια
@@ -61,9 +61,10 @@ CREATE TABLE IF NOT EXISTS "PUBLICATION" (
 	"genre-id" integer,
 	PRIMARY KEY ("isbn"),
 	FOREIGN KEY ("genre-id") REFERENCES "GENRE" ("id") --referential integrity constraint
-            ON UPDATE RESTRICT  
-            ON DELETE SET NULL --Δεν θέλουμε να διαγράψουμε ένα βιβλίο αν αυτό ανήκει σε κάποιο είδος, ακομα και αμ διαγραψουμε το ειδος
-);
+            ON UPDATE CASCADE --Αν αλλάξει το id ενός είδους, αλλάζει και στον πίνακα PUBLICATION   
+            ON DELETE SET NULL --Δεν θέλουμε να διαγράψουμε ένα βιβλίο αν αυτό ανήκει σε κάποιο είδος, ακομα και αν διαγραψουμε το ειδος
+);          --id ειδους μπορει να αλλαξει, αλλα δεν θελουμε να διαγραφεται το βιβλιο (δεν χρειαζονται αυστηροι περιορισοι οπως στα ΑΦΜ)
+            --To id ενός είδους μπορεί να αλλάξει και επιτρέπται να γίνει manually updated από τον χρήστη
 
 CREATE TABLE IF NOT EXISTS "client_orders" (
 	"Client_Tax_ID" integer,
@@ -74,11 +75,11 @@ CREATE TABLE IF NOT EXISTS "client_orders" (
 	"payment" float,
 	PRIMARY KEY ("Client_Tax_ID", "Publication-isbn"),
 	FOREIGN KEY ("Client_Tax_ID") REFERENCES "CLIENT" ("Tax_ID")
-            ON UPDATE RESTRICT
-            ON DELETE RESTRICT, -- Δεν θέλουμε να διαγράψουμε έναν πελάτη αν έχει κάνει παραγγελία, ωστε να διατηρήσουμε των συναλλαγών
+            ON UPDATE RESTRICT  --Δεν επιτρεπεται αλλαγη του Tax_Id του CLIENT
+            ON DELETE RESTRICT, -- Δεν θέλουμε να διαγράψουμε έναν πελάτη αν έχει κάνει παραγγελία, ωστε να διατηρήσουμε ιστορικο συναλλαγών
 	FOREIGN KEY ("Publication-isbn") REFERENCES "PUBLICATION" ("isbn")
-            ON UPDATE RESTRICT
-            ON DELETE RESTRICT
+            ON UPDATE RESTRICT  --Δεν επιτρεπεται αλλαγη του isbn του PUBLICATION
+            ON DELETE RESTRICT  --Δεν επιτρεπεται διαγραφη βιβλιου με isbn
 );
 
 CREATE TABLE IF NOT EXISTS "contributes" (
@@ -91,15 +92,15 @@ CREATE TABLE IF NOT EXISTS "contributes" (
 	PRIMARY KEY ("Partner_TaxId", "Publication-isbn"),
 	FOREIGN KEY ("Partner_TaxId") REFERENCES "PARTNER" ("Tax_Id") --referential integrity constraint
             ON UPDATE RESTRICT --Δεν θέλουμε να αλλάξουμε το Tax_Id του PARTNER οπότε το αποτρέπουμε με το RESTRICT
-            ON DELETE RESTRICT, -- κραταμε το tax_id ωστε να σχηματιζει Unique key και ας εχει διαγραφει ο συνεργατης
+            ON DELETE RESTRICT, -- κραταμε το tax_id ωστε να σχηματιζει Unique key, δεν επιτρεπεται διαγραφη συνεργατη
 	FOREIGN KEY ("Publication-isbn") REFERENCES "PUBLICATION" ("isbn")	--referential integrity constraint
             ON UPDATE RESTRICT --Δεν θέλουμε να αλλάξουμε το isbn του PUBLICATION οπότε το αποτρέπουμε με το RESTRICT
-            ON DELETE RESTRICT -- κραταμε το isbn ωστε να σχηματιζει Unique key και ας εχει διαγραφει το βιβλιο
+            ON DELETE RESTRICT -- κραταμε το isbn ωστε να σχηματιζει Unique key, δεν επιτρεπεται διαγραφη εντυπου
 );
 
 CREATE TABLE IF NOT EXISTS "order_printing_house" (
 	"Printing-id" integer ,
-	"Publication-isbn" integer, --
+	"Publication-isbn" integer, 
 	"order date" date,
 	"delivery date" date,
 	"quantity" integer,
@@ -108,11 +109,12 @@ CREATE TABLE IF NOT EXISTS "order_printing_house" (
 	PRIMARY KEY ("Printing-id", "Publication-isbn"),
 	FOREIGN KEY ("Printing-id") REFERENCES "PRINTING_HOUSE" ("p_id") --referential integrity constraint
             ON UPDATE RESTRICT --Δεν θέλουμε να αλλάξουμε το p_id του PRINTING_HOUSE οπότε το αποτρέπουμε με το RESTRICT
-            ON DELETE RESTRICT ,-- Retain the order but mark the printing house as removed
+            ON DELETE RESTRICT ,-- Retain the order, κρατάμε την παραγγελια δεν επιτρέπεται διαγραφή του τυπογραφειου
+			--ακομα και αν δεν συνεργαζομαστε πλεον με καποιο τυπογραφειο, παραμενει στην βαση για ιστορικους λογους
 	FOREIGN KEY ("Publication-isbn") REFERENCES "PUBLICATION" ("isbn")	--referential integrity constraint
             ON UPDATE RESTRICT 	--Δεν θέλουμε να αλλάξουμε το isbn του PUBLICATION οπότε το αποτρέπουμε με το RESTRICT
-            ON DELETE RESTRICT -- keep the order even if the publication is deleted 
-);                              -- book no longer offered, but we may need the order info/ transaction details
+            ON DELETE RESTRICT -- δεν διαγραφεται το εντυπο απο την βαση
+);                              -- ακομα και αν το βιβλιο δεν το προσφερουμε πια, θελουμε τις λεπτομερειες της συναλλαγης
 
 CREATE TABLE IF NOT EXISTS "communication-CLIENT" (
 	"Clinet_Tax_ID" integer,
@@ -121,7 +123,7 @@ CREATE TABLE IF NOT EXISTS "communication-CLIENT" (
 	FOREIGN KEY ("Clinet_Tax_ID") REFERENCES "CLIENT" ("Tax_ID") --referential integrity constraint
             ON UPDATE RESTRICT --Δεν θέλουμε να αλλάξουμε το Tax_Id του CLIENT οπότε το αποτρέπουμε με το RESTRICT
             ON DELETE CASCADE --Εάν διαγραφεί ένας CLIENT, όλες οι σχετικές εγγραφές στον πίνακα communication-CLIENT διαγράφονται αυτόματα.
-);
+);          -- ο πελατης μπορει να διαγραφει μόνο αν δεν εχει καμια σχεση με καποια παραγγελια
 
 CREATE TABLE IF NOT EXISTS "communication-PARTNER" (
 	"Partner_Tax_Id" integer,
@@ -130,7 +132,7 @@ CREATE TABLE IF NOT EXISTS "communication-PARTNER" (
 	FOREIGN KEY ("Partner_Tax_Id") REFERENCES "PARTNER" ("Tax_Id") --referential integrity constraint
             ON UPDATE RESTRICT --Δεν θέλουμε να αλλάξουμε το Tax_Id του PARTNER οπότε το αποτρέπουμε με το RESTRICT
             ON DELETE CASCADE --Εάν διαγραφεί ένας PARTNER, όλες οι σχετικές εγγραφές στον πίνακα communication-PARTNER διαγράφονται αυτόματα.
-);
+);                   -- Ο συνεργατης μπορει να διαγραφεί μόνο αν δεν εχει καμια σχεση με καποιο συμβολαιο ή συνεισφορα σε εντυπο (contributes)
 
 CREATE TABLE IF NOT EXISTS "communication-PRINTING" (
 	"Printing-id" integer,
@@ -140,5 +142,5 @@ CREATE TABLE IF NOT EXISTS "communication-PRINTING" (
             ON UPDATE RESTRICT --καθώς δεν πρόκειται να αλλάξουμε το πεδίο p_id από τον πίνακα PRINTING_HOUSE
             ON DELETE CASCADE --Εάν μια εγγραφή διαγραφεί από τον πίνακα PRINTING_HOUSE,
 			-- όλες οι σχετικές εγγραφές στον πίνακα communication-PRINTING διαγράφονται αυτόματα.
-);
+);  --το τυπογραφείο μπορεί να διαγραφεί μόνο αν δεν εχει καμια σχεση με καποια παραγγελια
 
