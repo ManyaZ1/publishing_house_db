897d50f77b267e151782c4280281405903e7c114
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-01-02 17:05:12 +0200

    Update generate_db_records.py

--- ourDB/generate_db_records.py
+++ ourDB/generate_db_records.py
@@ -639,7 +639,6 @@ class PublishingDatabaseManager:
 
 def main():
     print("SQLite version:", sqlite3.sqlite_version)
-    manager = PublishingDatabaseManager(scale_factor=10)
 
     manager.run()
 
