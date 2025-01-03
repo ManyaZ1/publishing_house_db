553cd6abdae72ef6ce22c24dbe68e59543e28372
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-01-03 16:41:26 +0200

    Update schema.sql

--- ourDB/schema.sql
+++ ourDB/schema.sql
@@ -56,7 +56,11 @@ CREATE TABLE IF NOT EXISTS "GENRE" (
 CREATE TABLE IF NOT EXISTS "PUBLICATION" (
 	"title" string,
 	"isbn" integer,
+<<<<<<< Updated upstream
 	"price" REAL CHECK(typeof(price) = 'real' AND price >= 0) ,  --float would let irregular values in the database
+=======
+	"price" REAL CHECK(typeof(price) = 'real' AND price >= 0),
+>>>>>>> Stashed changes
 	"stock" integer,
 	"genre-id" integer,
 	PRIMARY KEY ("isbn"),
