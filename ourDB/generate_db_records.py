e042cfc3e82b4f4dfde10702f9320aec407294f8
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-01-03 13:54:17 +0200

    Update generate_db_records.py

--- ourDB/generate_db_records.py
+++ ourDB/generate_db_records.py
@@ -685,7 +685,7 @@ class PublishingDatabaseManager:
         return;
 
 def main():
-    scale_factor = 100
+    scale_factor = 3
 
     print("SQLite version:", sqlite3.sqlite_version)
     manager = PublishingDatabaseManager(scale_factor = scale_factor)
